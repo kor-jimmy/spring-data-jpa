32ffca4e6911c72469d1c19540f996e58d727dfa
jintak.yoo <ascending77@naver.com>
2023-05-14 00:27:20 +0900

    auditing spring data jpa 활용

--- src/main/java/study/data/jpa/Application.java
+++ src/main/java/study/data/jpa/Application.java
@@ -2,12 +2,25 @@ package study.data.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // 임시로 UUID 사용, 실무에서는 세션에서 얻어오는 정보들로 사용
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 
 }
