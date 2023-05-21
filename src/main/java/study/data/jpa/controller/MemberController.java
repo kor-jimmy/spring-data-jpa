c89b2527522fdbf5bd237b4324d5fd27838f87f2
jintak.yoo <ascending77@naver.com>
2023-05-21 23:44:31 +0900

    page, sort, yml 설정 테스트

--- src/main/java/study/data/jpa/controller/MemberController.java
+++ src/main/java/study/data/jpa/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.data.jpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +30,20 @@ public class MemberController {
         return member.getUsername();
     }
 
+    // /members?page=0&size=5&sort=id,desc&sort=username,desc
+    // sort는 기본이 asc
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for(int i=0; i<100; i++){
+            memberRepository.save(new Member("user"+i, i));
+        }
+
     }
 
 }
